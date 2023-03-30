77853e7b5a53d5cf226bd1547b98972c390e96fa
zhenglin305 <66998320+zhenglin305@users.noreply.github.com>
2023-03-30 16:01:59 +0800

    Update yearning-wxwork.py

--- yearning-wxwork.py
+++ yearning-wxwork.py
@@ -1,20 +1,11 @@
-#!/usr/bin/env python
-# coding=utf-8
 from flask import Flask
 from flask import request
-from flask import make_response
-from flask import jsonify
-from urlparse import urlparse, parse_qs
-import time
-import sys
-import requests
 import json
+import requests
 
+REQUEST_ID_HEADER = 'x-fc-request-id'
 
 app = Flask(__name__)
-
-base_path = ''
-dedata = {'ret': 0, 'msg': "success"}
 
 # 企业微信群机器人
 
@@ -26,20 +17,12 @@ def send_wechat(md):
 
     r = requests.post(wechat_bot_url, data=json.dumps(request_data))
 
-@app.route('/', methods=['POST'])
-def home():
-    # 接收推过来的信息
-    a = str(request.get_data(as_text=True)) #原始推送过来的数据为dict格式，转换成str
-    b = a.replace('text','content')  #将text替换成content
-    c = eval(b) #转换成dict格式，不然无法推送消息
-    send_wechat(c)
-    return (jsonify(dedata))
+@app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def hello_world():
+        a = str(request.get_data(as_text=True)) #原始推送过来的数据
+        b = a.replace('text','content')  #将text替换成content
+        c = eval(b) #转换成dict格式，不然无法推送消息
+        send_wechat(c)
 
-
-def handler(environ, start_response):
-    parsed_tuple = urlparse(environ['fc.request_uri'])
-    li = parsed_tuple.path.split('/')
-    global base_path
-    if not base_path:
-        base_path = "/".join(li[0:5])
-    return app(environ, start_response)
+if __name__ == '__main__':
+        app.run(host='0.0.0.0',port=9000)
